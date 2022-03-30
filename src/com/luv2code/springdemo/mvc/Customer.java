b980c660298663f4131beff21387e00b604775f2
nischal <nischal.bhatt.87@gmail.com>
2022-03-30 22:29:08 +0800

    adding some for number validations

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,4 +1,6 @@
 package com.luv2code.springdemo.mvc;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -10,6 +12,10 @@ public class Customer {
 	@Size(min=1, message="is required la gundu")
 	private String lastName;
 
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private int freePasses;
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -24,6 +30,14 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
 	}
 	
 	
