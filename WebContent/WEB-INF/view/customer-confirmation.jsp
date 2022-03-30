31a2d4f363dbb58283f8d60e6746a45cb47c52ed
nischal <nischal.bhatt.87@gmail.com>
2022-03-30 23:41:05 +0800

    custom annotations!

--- WebContent/WEB-INF/view/customer-confirmation.jsp
+++ WebContent/WEB-INF/view/customer-confirmation.jsp
@@ -15,6 +15,9 @@
       Free passes: ${customer.freePasses}
       <br><br>
       Postal Code: ${customer.postalCode}
+      <br><br>
+      Course Code: ${customer.courseCode}
+      <br><br>
    </body>
 
 </html>
--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -35,6 +35,10 @@ public class CustomerController {
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
 
 		System.out.println(theCustomer.getLastName());
+		System.out.println("Binding result: "+ theBindingResult);
+		System.out.println("\n\n\n\n\n");
+		
+		
 		if (theBindingResult.hasErrors()) {
               return "customer-form";
 		} else {
